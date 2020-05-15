27e5f016f611bb493bdd5e5d61b9bb9e6fbfaeeb
Alpha-Beta-Charlie <63340567+Alpha-Beta-Charlie@users.noreply.github.com>
2020-05-15 14:46:25 +0200

    slt

--- TP-CPP-git/controlleur.cpp
+++ TP-CPP-git/controlleur.cpp
@@ -1,1 +1,44 @@
-#include "controleur.h"
+#include "controlleur.h"
+
+controlleur::controlleur()
+	:menu(0),choix(0)
+{}
+void controlleur::setchoix(int _choix)
+{}
+void controlleur::instruction()
+{
+	voirImage im;
+	if (menu == 0){}
+	else if (menu == 1){}
+	else if (menu == 2){}
+	else if (menu == 3){
+		//choix dans se menu...
+		if (choix == 1) {
+
+		}
+		else if (choix == 2) {
+
+		}
+	}
+	else if (menu == 4){}
+	else if (menu == 5){
+		//choix dans se menu...
+		if (choix == 1) {
+
+		}
+		else if (choix == 2) {
+
+		}
+	}
+	else if (menu == 6){}
+	else if (menu == 7){
+		//choix dans se menu...
+		if (choix == 1) {
+
+		}
+		else if (choix == 2) {
+
+		}
+	}
+
+}
--- TP-CPP-git/controlleur.h
+++ TP-CPP-git/controlleur.h
@@ -1,13 +1,19 @@
 
 #pragma once
 #include <iostream>
+#include "voirImage.h"
 # include <opencv2/opencv.hpp>
 #include "opencv2/imgproc.hpp"
 #include "opencv2/imgcodecs.hpp"
 #include "opencv2/highgui.hpp"
 class controlleur
 {
+private:
+	int menu;
+	int choix;
+public:
 	controlleur();
-	choix 
+	void setchoix(int _choix);
+	void instruction();
 };
 
